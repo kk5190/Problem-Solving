8653f8e42b01e6beb54a690b278fd64fc0338c24
krishna <kk5190krrish@gmail.com>
2022-08-31 18:53:20 +0530

    Time: 772 ms (57.14%), Space: 21.9 MB (78.57%) - LeetHub

--- 1537-get-the-maximum-score/1537-get-the-maximum-score.py
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.py
@@ -1,35 +1,37 @@
 class Solution(object):
-    
+    MODULO_AMT = 10 ** 9 + 7
     def maxSum(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: int
         """
-        MODULO = 10 ** 9 + 7
-        i = j = 0
-        lenNums1 = len(nums1)
-        lenNums2 = len(nums2)
-        sumNums1 = 0
-        sumNums2 = 0
+        len1, len2 = len(nums1), len(nums2)
+        sum1, sum2 = 0, 0
+        i, j = 0, 0
         ans = 0
-        
-        while i < lenNums1 or j < lenNums2:
-            if i < lenNums1 and j < lenNums2 and nums1[i] == nums2[j]:
-                ans += max(sumNums1, sumNums2) + nums1[i]
-                ans %= MODULO
-                sumNums1 = 0
-                sumNums2 = 0
+        while i < len1 and j < len2:
+            if nums1[i] < nums2[j]:
+                sum1 += nums1[i]
                 i += 1
-                j += 1
-                continue
-            if i == lenNums1 or (j != lenNums2 and nums1[i] >nums2[j]):
-                sumNums2 += nums2[j]
+            elif nums1[i] > nums2[j]:
+                sum2 += nums2[j]
                 j += 1
             else:
-                sumNums1 += nums1[i]
+                ans += max(sum1, sum2) + nums1[i]
+                ans % self.MODULO_AMT
+                sum1 = 0
+                sum2 = 0
                 i += 1
-        ans += max(sumNums1, sumNums2)
-        return ans % MODULO
+                j += 1
+        while i < len1:
+            sum1 += nums1[i]
+            i += 1
+        while j < len2:
+            sum2 += nums2[j]
+            j += 1
+
+        ans += max(sum1, sum2)
+        return ans % self.MODULO_AMT
                 
         
