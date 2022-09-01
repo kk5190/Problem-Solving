4ff03049610ba9c99660d7c9e256faef7518a1c6
krishna <kk5190krrish@gmail.com>
2022-09-02 00:30:04 +0530

    Time: 1114 ms (5.07%), Space: 25.7 MB (87.59%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.py
@@ -5,14 +5,13 @@ class Solution(object):
         :rtype: int
         """
         start, end = 0, len(arr) - 1
-        ans = -1
         while start <= end:
             mid = (start + end) // 2
             if arr[mid] > arr[mid+1]:
                 ans = mid
                 end = mid - 1
             else:
-                start = start + 1
+                start = mid + 1
         return ans
                 
         
