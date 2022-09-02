01be2b16c491a982ef90eac2e495c51dd827f62e
krishna <kk5190krrish@gmail.com>
2022-09-03 01:41:06 +0530

    Time: 530 ms (68.79%), Space: 15.1 MB (95.30%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -5,28 +5,24 @@ class Solution(object):
         :type days: int
         :rtype: int
         """
-        def feasible(weights, max_weight, d):
-            req_days = 1
-            capacity = max_weight
-            i = 0
-            n = len(weights)
-            while i < n:
-                if weights[i] <= capacity:
-                    capacity -= weights[i]
-                    i += 1
-                else:
-                    req_days += 1
-                    capacity = max_weight
-            return req_days <= d
-
         min_ptr = max(weights)
         max_ptr = sum(weights)
         boundary_index = max_ptr
         while min_ptr <= max_ptr:
             midpoint = (min_ptr + max_ptr) // 2
-            if feasible(weights, midpoint, days):
+            if self.feasible(weights, midpoint, days):
                 boundary_index = midpoint
                 max_ptr = midpoint - 1
             else:
                 min_ptr = midpoint + 1
         return boundary_index
+    
+    def feasible(self, weights, max_weight, d):
+            req_days = 1
+            current_weight = 0
+            for weight in weights:
+                current_weight += weight
+                if current_weight > max_weight:
+                    req_days += 1
+                    current_weight = weight
+            return req_days <= d
