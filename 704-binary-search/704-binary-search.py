ea215c6d0497cc9eda9c8576446ca732ba410141
krishna <kk5190krrish@gmail.com>
2022-09-03 15:34:26 +0530

    Time: 278 ms (58.76%), Space: 14.4 MB (98.51%) - LeetHub

---
+++ 704-binary-search/704-binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if target > nums[mid]:
+                left = mid + 1
+            elif target < nums[mid]:
+                right = mid - 1
+            else:
+                return mid
+        return -1
+        
