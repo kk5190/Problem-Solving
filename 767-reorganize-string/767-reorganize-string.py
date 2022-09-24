28bab7953ae8ff35c19095bacda2c7ffee898f36
krishna <kk5190krrish@gmail.com>
2022-09-24 23:47:29 +0530

    Time: 53 ms (60.96%), Space: 13.9 MB (29.47%) - LeetHub

---
+++ 767-reorganize-string/767-reorganize-string.py
@@ -0,0 +1,26 @@
+from heapq import heapify, heappop
+from typing import Counter
+
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        n = len(s)
+        str_count = Counter(s)
+        pq = [(-value, key) for key, value in str_count.items()]
+        heapify(pq)
+        
+        if -pq[0][0] > (n + 1) // 2:
+            return ""
+        res = [None] * n
+        
+        pointer = 0
+        while len(pq) > 0:
+            (count, char) = heappop(pq)
+            count = -count
+            for _ in range(count):
+                res[pointer] = char
+                pointer += 2
+                if pointer >= n:
+                    pointer = 1
+        return ''.join(res)
+        
+        
